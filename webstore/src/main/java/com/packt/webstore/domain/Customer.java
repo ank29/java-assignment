cd40a0e87f002b563e2a9a31e0fe676b260b7905
ank29 <ankita290995@gmail.com>
2016-11-07 17:35:18 +0530

    add

---
+++ webstore/src/main/java/com/packt/webstore/domain/Customer.java
@@ -0,0 +1,8 @@
+package com.packt.webstore.domain;
+
+import java.math.BigDecimal;
+private int productId; 
+	private String name;
+	private BigDecimal unitPrice;
+	private String description;
+}
---
+++ webstore/src/main/java/com/packt/webstore/service/ProductService.java
@@ -0,0 +1,7 @@
+package com.packt.webstore.service;
+import java.util.List;
+
+import com.packt.webstore.domain.Product;
+public interface ProductService {
+	List<Product> getAllProducts();
+}
